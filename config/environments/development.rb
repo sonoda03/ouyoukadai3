8d8d3ab71a02262656eb0dda9a8e3594c7ad693e
EC2 Default User <ec2-user@ip-172-31-47-249.ap-northeast-1.compute.internal>
2022-09-26 01:42:38 +0000

    MigrationError解決前

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,6 @@ Rails.application.configure do
   config.hosts << "49e69874be6d4ef3a0718e9b15430b98.vfs.cloud9.us-east-1.amazonaws.com"
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  config.hosts << "aaea5f05a5cc4e19b2c78df08cf61327.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
